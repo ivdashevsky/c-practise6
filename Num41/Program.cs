95217f96a05d0aa45456263610c2eb9aeb83fda9
Ilya <iii@ya.ru>
2022-07-16 15:06:57 +0500

    tasks done

---
+++ Num41/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("\nВведите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+if (M <= 0) 
+{ 
+    Console.WriteLine("Число не соответствует количеству элементов массива"); 
+}
+else
+{
+    int[] arr = new int[M];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"\nВведите число {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(String.Join(" ", arr));
+    Console.WriteLine($"Кол-во чисел > 0: {count}");
+}
---
+++ Num43/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("\nВведите число b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nВведите число k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nВведите число b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("\nВведите число k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y1 = k1*x + b1;
+
+Console.WriteLine($"{x},{y1}");
